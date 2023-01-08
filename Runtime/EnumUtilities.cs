e3fc5c8c1e90723d216dea6ac59843edd433927a
Jacob Dunbar <jacobfdunbar@gmail.com>
2023-01-08 10:09:04 -0500

    Adding EnumUtilities

---
+++ Runtime/EnumUtilities.cs
@@ -0,0 +1,21 @@
+using System;
+
+public static class EnumUtilities
+{
+    public static T Next<T>(this T initialValue) where T : Enum
+    {
+        return initialValue.Shift(1);
+    }
+
+    public static T Previous<T>(this T initialValue) where T : Enum
+    {
+        return initialValue.Shift(-1);
+    }
+
+    public static T Shift<T>(this T initialValue, int amount) where T : Enum
+    {
+        var enumValues = (T[]) Enum.GetValues(typeof(T));
+        var index = (Array.IndexOf(enumValues, initialValue) + amount) % enumValues.Length;
+        return enumValues[index];
+    }
+}
